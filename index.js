1c9d106ebe4e17cfeb4a16f2373f2bf074dae46a
szikaria961 <szikaria961@gmail.com>
2020-11-01 14:20:27 -0500

    Implement places API integration

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+const { writeFileSync } = require('fs');
+const axios = require('axios');
+const { data: poutineLocations } = require('./export.json');
+
+const API_KEY = process.env.API_KEY;
+const API_BASE = 'https://maps.googleapis.com/maps/api/place/textsearch/';
+const EXPORT_PATH = "export.json";
+
+const buildQuery = ({
+  format = 'json',
+  input,
+  inputType = 'textquery',
+  fields = 'formatted_address,name'
+}) => {
+  return `${API_BASE}${format}?input=${encodeURI(input)}&inputtype=${inputType}&fields=${fields}&key=${API_KEY}`;
+}
+
+async function main() {
+  try {
+    const { data } = await axios.get(buildQuery({
+      input: 'Poutine places in toronto'
+    }));
+    writeFileSync(EXPORT_PATH, JSON.stringify(data));
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+main();
