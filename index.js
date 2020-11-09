b52828797f723139c84741271aadaa27114aa484
szikaria961 <szikaria961@gmail.com>
2020-11-09 12:55:02 -0500

    Integrate google maps in front-end

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
 const { writeFileSync } = require('fs');
+const _ = require('lodash');
 const axios = require('axios');
 const express = require('express');
 const { data: poutineLocations } = require('./data.json');
 const { buildQuery } = require('./helpers');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
+const API_KEY = process.env.API_KEY;
 const PLACES_API_INPUT = "poutine places in Toronto";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/static'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.listen(PORT, () => {
   console.log(`[ index.js ] Listening on port ${PORT}`);
 });
 
+app.get('/', (req, res) => {
+  res.render('index.html', { API_KEY })
+});
+
 app.get('/api/places', (req, res, next) => {
   try {
-    const data = poutineLocations.map(({ name, formatted_address: address }) => {
+    const data = poutineLocations.map(({ name, formatted_address: address, geometry }) => {
       return {
         name,
-        address
+        address,
+        geometry
       };
     });
     res.json(data);
@@ -37,6 +48,18 @@ app.get('/api/places-from-resource', async (req, res, next) => {
       input: PLACES_API_INPUT
     }));
     res.json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/places/:name', (req, res, next) => {
+  const { name } = req.params;
+
+  try {
+    const data = _.find(poutineLocations, { name });
+
+    res.json([data.name, data.formatted_address]);
   } catch (err) {
     next(err);
   }
---
+++ static/scripts.js
@@ -0,0 +1,48 @@
+function loadAllPlaces(callback) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      callback(response);
+    }
+  };
+  xhttp.open("GET", "/api/places", true);
+  xhttp.send();
+}
+
+function initMap() {
+  var options = {
+    zoom: 14,
+    center: { lat: 43.651070, lng: -79.347015 },
+  }
+
+  loadAllPlaces(function(response){
+    var map = new google.maps.Map(document.getElementById("map"), options);
+
+    response.forEach(function(item){
+      addMarkers({
+        coords: {
+          lat: item.geometry.location.lat, lng: item.geometry.location.lng
+        },
+        name: item.name,
+        address: item.address
+      });
+    });
+
+    function addMarkers(locationData){
+      var marker = new google.maps.Marker({
+        position: locationData.coords,
+        map: map
+      });
+
+      var infoWindow = new google.maps.InfoWindow({
+        content:`<p>Name: ${locationData.name}</p>
+                 <p>Address: ${locationData.address}</p>`
+      });
+
+      marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+      });
+    }
+  });
+}
